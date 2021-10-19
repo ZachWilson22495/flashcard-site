de83b5890919689bbe49d5c494bbf321d51fca88
Anna Pittman <annaepittman@gmail.com>
2021-10-19 15:59:25 -0700

    add html, css, js files.
    Co-authored by: Zach Wilson <zwilson22495@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,96 @@
+$(document).ready(function() {
+  $(".definition1").click(function() {
+    $(".definition1-showing").toggle();
+    $(".definition1-hidden").toggle();
+  });
+
+  $(".definition2").click(function() {
+    $(".definition2-showing").toggle();
+    $(".definition2-hidden").toggle();
+  });
+
+  $(".definition3").click(function() {
+    $(".definition3-showing").toggle();
+    $(".definition3-hidden").toggle();
+  });
+
+  $(".definition4").click(function() {
+    $(".definition4-showing").toggle();
+    $(".definition4-hidden").toggle();
+  });
+
+  $(".definition5").click(function() {
+    $(".definition5-showing").toggle();
+    $(".definition5-hidden").toggle();
+  });
+
+  $(".definition6").click(function() {
+    $(".definition6-showing").toggle();
+    $(".definition6-hidden").toggle();
+  });
+
+  $(".definition7").click(function() {
+    $(".definition7-showing").toggle();
+    $(".definition7-hidden").toggle();
+  });
+
+  $(".definition8").click(function() {
+    $(".definition8-showing").toggle();
+    $(".definition8-hidden").toggle();
+  });
+
+  $(".definition9").click(function() {
+    $(".definition9-showing").toggle();
+    $(".definition9-hidden").toggle();
+  });
+
+  $(".definition10").click(function() {
+    $(".definition10-showing").toggle();
+    $(".definition10-hidden").toggle();
+  });
+
+  $(".definition11").click(function() {
+    $(".definition11-showing").toggle();
+    $(".definition11-hidden").toggle();
+  });
+
+  $(".definition12").click(function() {
+    $(".definition12-showing").toggle();
+    $(".definition12-hidden").toggle();
+  });
+
+  $(".definition13").click(function() {
+    $(".definition13-showing").toggle();
+    $(".definition13-hidden").toggle();
+  });
+
+  $(".definition14").click(function() {
+    $(".definition14-showing").toggle();
+    $(".definition14-hidden").toggle();
+  });
+
+  $(".definition15").click(function() {
+    $(".definition15-showing").toggle();
+    $(".definition15-hidden").toggle();
+  });
+
+  $(".definition16").click(function() {
+    $(".definition16-showing").toggle();
+    $(".definition16-hidden").toggle();
+  });
+
+  $(".definition17").click(function() {
+    $(".definition17-showing").toggle();
+    $(".definition17-hidden").toggle();
+  });
+
+  $(".definition18").click(function() {
+    $(".definition18-showing").toggle();
+    $(".definition18-hidden").toggle();
+  });
+
+  $(".definition19").click(function() {
+    $(".definition19-showing").toggle();
+    $(".definition19-hidden").toggle();
+  });
+});
